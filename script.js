8267674e81c7598283862ba5ae66f9ee48f494a8
Aaron Anglin <aaron.anglin101@gmail.com>
2023-01-23 11:03:54 -0600

    fixed scaling and added buttons side by side

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-const screen = document.querySelector('.screen');
-const container = document.querySelector('.buttons');
+const screen = document.querySelector(".screen");
+const container = document.querySelector(".buttons");
 
 let calculation = [];
 let stringCalculation = "";
 
-container.addEventListener('click', (e) => {
-    if (e.target.tagName === "BUTTON") {
-        const value = e.target.textContent;
-        if (value === "CLEAR") {
-            calculation = [];
-            stringCalculation = "";
-            screen.textContent = '0';
-        } else if (value === "=") {
-            stringCalculation = calculation.reduce((acc, cur) => acc + cur);
-            let result = new Function('return ' + stringCalculation)();
-            // Checking to see if the the results has digits after the decimal point. If it does then the first code will run.
-            if (result.toString().indexOf(".") != -1) {
-                screen.textContent = (result).toFixed(5);
-                // if it doesn't have digits then it will run this code.
-            } else {
-                screen.textContent = result;
-            }
-        }else if (value === "%") {
-            let currentNum = '';
-            for(let i=calculation.length-1; i>=0; i--) {
-                if(isNaN(calculation[i]) && calculation[i] !== '.') {
-                    break;
-                }
-                currentNum = calculation[i] + currentNum;
-            }
-            currentNum = parseFloat(currentNum);
-            currentNum /= 100;
-            calculation.splice(calculation.indexOf(currentNum) + 1);
-            calculation.push(currentNum);
-            stringCalculation = calculation.reduce((acc, cur) => acc + cur);
-            screen.textContent = stringCalculation;
-        } else {
-            calculation.push(value);
-            stringCalculation = calculation.reduce((acc, cur) => acc + cur);
-            screen.textContent = stringCalculation;
+container.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const value = e.target.textContent;
+    if (value === "CLEAR") {
+      calculation = [];
+      stringCalculation = "";
+      screen.textContent = "0";
+    } else if (value === "=") {
+      stringCalculation = calculation.reduce((acc, cur) => acc + cur);
+      let result = new Function("return " + stringCalculation)();
+      // Checking to see if the the results has digits after the decimal point. If it does then the first code will run.
+      if (result.toString().indexOf(".") != -1) {
+        screen.textContent = result.toFixed(5);
+        // if it doesn't have digits then it will run this code.
+      } else {
+        screen.textContent = result;
+      }
+    } else if (value === "%") {
+      let currentNum = "";
+      for (let i = calculation.length - 1; i >= 0; i--) {
+        if (isNaN(calculation[i]) && calculation[i] !== ".") {
+          break;
         }
+        currentNum = calculation[i] + currentNum;
+      }
+      currentNum = parseFloat(currentNum);
+      currentNum /= 100;
+      calculation.splice(calculation.indexOf(currentNum) + 1);
+      calculation.push(currentNum);
+      stringCalculation = calculation.reduce((acc, cur) => acc + cur);
+      screen.textContent = stringCalculation;
+    } else {
+      calculation.push(value);
+      stringCalculation = calculation.reduce((acc, cur) => acc + cur);
+      screen.textContent = stringCalculation;
     }
+  }
 });
 
 // The time complexity of the above code is O(1) , this is because regardless of the number of buttons or actions performed on them, the code only performs a constant number of operations each time a button is clicked.
